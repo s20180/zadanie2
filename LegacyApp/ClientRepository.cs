24b755b488e7341158d789a6b08fe4dfdc329c69
Savren <ptrgls94@gmail.com>
2024-04-08 19:35:32 +0200

    Added missing client for LegacyAppConsumer

--- LegacyApp/ClientRepository.cs
+++ LegacyApp/ClientRepository.cs
@@ -16,7 +16,8 @@ namespace LegacyApp
             {3, new Client{ClientId = 3, Name = "Smith", Address = "Warszawa, Kolorowa 22", Email = "smith@gmail.pl", Type = new ImportantClient()}},
             {4, new Client{ClientId = 4, Name = "Doe", Address = "Warszawa, Koszykowa 32", Email = "doe@gmail.pl", Type = new ImportantClient()}},
             {5, new Client{ClientId = 5, Name = "Kwiatkowski", Address = "Warszawa, Złota 52", Email = "kwiatkowski@wp.pl", Type = new NormalClient()}},
-            {6, new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = new NormalClient()}}
+            {6, new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = new NormalClient()}},
+            {100, new Client{ClientId = 100, Name = "Doe", Address = "Warszawa, Koszykowa 52", Email = "johndoe@gmail.com", Type = new VeryImportantClient()}}
         };
         
         public ClientRepository()
